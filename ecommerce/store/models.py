31a1d6398720ec85c761edb51b21cd40ae71c908
zipporia <markchristophercornelio@gmail.com>
2021-02-22 23:34:04 +0800

    models order update

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -17,3 +17,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_order = models.DateTimeField(auto_now_add=True)
+    complete = models.Booleanfield(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
