874183c2cb80d987246b888e8df180fe29d7f8a5
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2021-02-23 23:48:44 +0800

    order item

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    #image
 
     def __str__(self):
         return self.name
@@ -26,3 +27,7 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
