4cfcd3dfb0e493ac1c2c7116f79ba6d0e44f6223
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2021-02-25 12:54:28 +0800

    update

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -37,7 +37,7 @@ class OrderItem(models.Model):
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
